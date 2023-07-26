8ee5dccebed027e28d5c8ecb65caa79f5336a8ca
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2023-07-26 14:56:43 +1200

    Add files via upload

--- get_recipe_name_and_size_V1.py
+++ get_recipe_name_and_size_V1.py
@@ -1,7 +1,4 @@
-
-#functions here
-
-
+# functions here
 def not_blank(question, error):
     valid = False
     while not valid:
@@ -18,6 +15,8 @@ def num_check(question, error, num_type):
     valid = False
     while not valid:
 
+        error = "please enter a number that is more than zero"
+
         try:
             response = num_type(input(question))
 
@@ -30,6 +29,28 @@ def num_check(question, error, num_type):
             print(error)
 
 
+def get_ingredients():
+    get_recipe = []
+
+    stop = ""
+
+    print("enter ingredients one at a time, enter <xxx> to finish")
+
+    while stop != "xxx":
+        # get ingredient
+        get_ingredient = not_blank("what is your ingredient (enter <xxx> to end): ")
+
+        # stop looping if <xxx> is entered and there are 3 or more ingredients entered
+        if get_ingredient == "xxx" and len(get_recipe) > 2:
+            break
+
+        elif get_ingredient == "xxx" and len(get_recipe) <3:
+            print("Sorry you need at least 3 ingredients to proceed.")
+
+        else:
+            get_recipe.append(get_ingredient)
+
+
 # ***** user input goes here *****
 
 # get recipe name
@@ -37,6 +58,11 @@ recipe_name = not_blank("What is the name of your recipe?",
                         "sorry the recipe name cannot be blank")
 
 # get serving size
-serve_size = num_check("what is the serving size of your recipe?")
-needed_size = num_check("")
+serve_size = num_check("what is the serving size of your recipe?", "sorry this cant be blank")
+needed_size = num_check("how many people are you serving for?", "sorry this cant be more than zero", int)
+print()
+
+
+
+
 
