be88b3c4424e1d0fa0c9b9d944d69388e5587ac7
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2023-08-18 12:28:57 +1200

    Add files via upload

--- 01_Base_V2.py
+++ 01_Base_V2.py
@@ -2,7 +2,7 @@ import pandas
 
 
 # functions go here
-
+# makes sure that when the user inputs a number it is not less than zero
 def num_check(question, error, num_type):
     valid = False
     while not valid:
@@ -19,6 +19,7 @@ def num_check(question, error, num_type):
             print(error)
 
 
+# allows the user to only input variations of "yes" and "no" in response to a question
 def yes_no(question):
     valid = False
     while not valid:
@@ -36,6 +37,7 @@ def yes_no(question):
             print("please answer yes / no")
 
 
+# makes sure the user cannot input a blank answer in response to a question
 def not_blank(question, error):
     valid = False
     while not valid:
@@ -49,6 +51,7 @@ def not_blank(question, error):
 
 
 # main routine goes here
+# asks user if they want to see the instructions
 want_instructions = yes_no("Do you want to see the instructions? ")
 
 if want_instructions == "yes":
@@ -71,7 +74,7 @@ if want_instructions == "yes":
     \n
     ''')
 
-# main routine
+# main routine here
 
 ingredient_name = []
 total_ingredient_price = []
@@ -88,12 +91,12 @@ recipe_dict = {
 }
 
 # ***** user input goes here *****
-# get recipe name
+# asks user what the name of their recipe is and makes sure it cannot be blank
 recipe_name = not_blank("What is the name of your recipe?",
                         "sorry the recipe name cannot be blank")
 print()
-# get serving size
-needed_size = num_check("how many people are you serving?", "sorry this cant be more than zero", int)
+# asks user for the amount of people they are serving for and makes sure the answer is more than zero
+needed_size = num_check("how many people are you serving?", "sorry this must be more than zero", int)
 print()
 
 needed_amount = ""
@@ -105,8 +108,8 @@ while True:
 
     print("enter ingredients one at a time, enter <xxx> to finish")
 
-    # get ingredient
-    get_ingredient = not_blank("what is your ingredient (enter <xxx> to end): ", "please enter a valid response")
+    # asks user what their ingredient is
+    get_ingredient = not_blank("what is your ingredient (enter <xxx> to end): ", "sorry this cannot be blank")
     print()
     # stop looping if <xxx> is entered and there are 3 or more ingredients entered
     if get_ingredient == "xxx" and len(ingredient_name) > 1:
@@ -115,15 +118,18 @@ while True:
     elif get_ingredient == "xxx" and len(ingredient_name) < 3:
         print("Sorry you need at least 3 ingredients to proceed.")
 
-    needed_amount = num_check("How much of the ingredient do you need? (in grams): ", "please enter a number", float)
+    # asks user how much of the ingredient they need,
+    # how much of the ingredient they have bought,
+    # and how much they paid for the ingredient
+    needed_amount = num_check("How much of the ingredient do you need? (in grams): ", "please enter a number more than zero", float)
     print()
-    bought_amount = num_check("How much have you bought? (in grams): ", "please enter a number", float)
+    bought_amount = num_check("How much have you bought? (in grams): ", "please enter a number more than zero", float)
     print()
-    ingredient_price = num_check("How much did you pay for the ingredient?: $", "please enter a number", float)
+    ingredient_price = num_check("How much did you pay for the ingredient?: $", "please enter a number more than zero", float)
     print()
     unit_cost = (ingredient_price / bought_amount * needed_amount)
 
-    # list list list list
+    # list
     ingredient_name.append(get_ingredient)
     total_ingredient_price.append(ingredient_price)
     total_needed_amount.append(needed_amount)
@@ -133,7 +139,8 @@ while True:
 # panda the goods
 recipe_panda = pandas.DataFrame(recipe_dict)
 
-# cost of each serving and total cost
+# calculate the cost of each serving and total cost
+# and then print the total cost and cost per serving to 2dp
 total_cost = recipe_panda["*Production Cost*"].sum()
 serving_cost = total_cost / needed_size
 print(recipe_panda)
